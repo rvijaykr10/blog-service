d87af65c7640e3773acc8ced7706b9c86e71a044
vijaykumar <vijaykumar@vijaykumars-MacBook-Air.local>
2024-08-17 23:19:25 +0530

    removed comments

--- routers/auth.py
+++ routers/auth.py
@@ -101,7 +101,6 @@ async def create_user(db:db_dependency, create_user_request: CreateUserRequest):
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                  db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
-    print('-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n', user)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Could not validate user.')
